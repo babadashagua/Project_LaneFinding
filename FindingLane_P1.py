8d8c5473ee810fd15a3e93ddb87cd0d2326f32e0
Yu Cheng <chengyu.sdu@gmail.com>
2019-02-26 20:02:09 -0500

    Update FindingLane_P1.py

--- FindingLane_P1.py
+++ FindingLane_P1.py
@@ -13,7 +13,8 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
-# functions used for lane finding
+# functions used for lane finding, except the hough_lines() and lane_finding() functions, other helper functions I used are from 
+# this project introduction.
 def grayscale(img):
     
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
